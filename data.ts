21a0689cd411785a5de3d85e9500f4de99ff6d0b
jirka840yt <jirka840cz@gmail.com>
2026-05-27 14:34:26 +0200

    vytvoreni app.ts a pridani dat

---
+++ data.ts
@@ -0,0 +1,11 @@
+/**
+ * Soubor: data.ts
+ * Obsahuje pouze surová data (např. simulace odpovědi z databáze).
+ */
+
+const surovaDataVozidel = [
+    { typ: "osobni", spz: "1A1 1234", zakladniCena: 800, jeLuxusni: false },
+    { typ: "osobni", spz: "2B2 5678", zakladniCena: 1500, jeLuxusni: true },
+    { typ: "dodavka", spz: "3C3 9012", zakladniCena: 1200, nosnostKg: 800 },
+    { typ: "dodavka", spz: "4D4 3456", zakladniCena: 2000, nosnostKg: 1600 }
+];
